b743ec41eac880c4b44fcd80c22d71f223cdcc17
Denys Karakoi <denys.karakoi@gmail.com>
2020-12-25 17:40:53 +0100

    дз 3

---
+++ task4.js
@@ -0,0 +1,28 @@
+
+const countTotalSalary = function(employees) {
+
+    const values = Object.values(employees);
+     let sum = 0;
+     for (const value of values) {
+       sum += value;
+     }
+     return sum;
+    };
+  
+  console.log(countTotalSalary({})); // 0
+  
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
+  
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
